888972928c316993a40feb7589c11e561aab248b
Romain SOMMERARD <Romain@mbp-de-romain-2.home>
2014-03-01 13:39:06 +0100

    h140301
    Bug corrected. Added folder path and file name vars.

--- wathcitf.py
+++ wathcitf.py
@@ -1,12 +1,13 @@
 #!/usr/bin/Python3
 #
-#   WATHCITF script : What are the hexa colors in this file ?
+#   Wathcitf script : What are the hexa colors in this file ?
 #
 #   Romain (BucOder) SOMMERARD
 #   http://www.bucoder.com
-#   Distributed under the GPL version 3 licence
+#   Distributed under the GPL version 3 licence.
 #
-#   Search all hexa color in the css file
+#   Search all hexa colors in the css file.
+#   Script for Linux system.
 #
 #   Usage:  Replace the PATH_FILE by the path of
 #           your file name and run this script.
@@ -15,33 +16,50 @@
 #		
 #		OR      
 #
+#               If python 3 is the only version of python in your computer.
 #		# Python wathcitf
 #
 #		OR
 #
+#               You can run the script like application.
 #		# chmod +x wathcitf
 #               # ./wathcitf
 #
 
 import re
+import os
 
-# The path of css file
-PATH_FILE = ''
 
-if PATH_FILE == '':
-    print('Error: PATH_FILE empty')
+# The path of folder where is the file.
+FOLDER_PATH = ''
+
+# The css file name.
+FILE_NAME = ''
+
+
+if FOLDER_PATH == '':
+    print('Error: FOLDER_PATH empty')
     exit(1)
+
+if FILE_NAME == '':
+    print('Error: FILE_NAME empty')
+    exit(0)
 
 colors = set()
 
-expression = r'(#[0-9a-fA-F]{3,6})'
+expression = r'(.*)(#[0-9a-fA-F]{3,6})(.*)'
 
-with open(PATH_FILE, 'r') as file:
-    for line in file:
-        color = re.match(expression, line)
-        if color:
-            colors.add(color.group(0))
-            
+os.system('cd ' + FOLDER_PATH)
+
+try:
+    with open(FILE_NAME, 'r') as file:
+        for line in file:
+            color = re.match(expression, line)
+            if color:
+                colors.add(color.group(2))
+except FileNotFoundError:
+    print('Error: File not found (',PATH_FILE,')',sep='')
+    exit(1)            
 
 for color in colors:
     print(color)
